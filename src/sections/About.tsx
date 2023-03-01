15cea957de79d357cd2b4742f097fd4ccb6f62c1
Isaac Blanco <nuclearhands2001@gmail.com>
2023-03-01 00:59:40 -0500

    Mobile working, animation still mising and change photos

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -2,25 +2,33 @@ import profile from "../assets/images/other/profile.jpg";
 
 const About = () => {
     return (
-        <div className="about-section">
+        <div className="section about-section">
             <h2 className="text-section">About</h2>
             <h3 className="inner-header">Who is Isaac?</h3>
             <div className="about-wrapper">
                 <img src={profile} alt="profile-pic"></img>
                 <div className="about-text">
                     <p>
-                    I am passionate about both the functionality and visualization of bringing an idea to life. 
-                    The experience, its usability and how you feel plays a fundamental role in how much you 
-                    use an application. And I completely agree with the phrase "You don't need more space, you need less stuff."
-                    </p>
-                    <p>I'm a senior studying computer science at the University of the North 🇨🇴, I have a diploma 
-                    in data science from ESAP, and a certification in data science from Correlation One's DS4A
-                    program (Yes, one of my biggest interests is data science🤫).
+                        I am passionate about both the functionality and
+                        visualization of bringing an idea to life. The
+                        experience, its usability and how you feel plays a
+                        fundamental role in how much you use an application. And
+                        I completely agree with the phrase "You don't need more
+                        space, you need less stuff."
                     </p>
                     <p>
-                    I am happy researching, looking for new ideas and learning new ideas. I am currently 
-                    working as a fullstack developer focused on the frontend. I am not available for freelancer 
-                    work, but you can contact me anyway and say hello. 
+                        I'm a senior studying computer science at the University
+                        of the North 🇨🇴, I have a diploma in data science from
+                        ESAP, and a certification in data science from
+                        Correlation One's DS4A program (Yes, one of my biggest
+                        interests is data science🤫).
+                    </p>
+                    <p>
+                        I am happy researching, looking for new ideas and
+                        learning new ideas. I am currently working as a
+                        fullstack developer focused on the frontend. I am not
+                        available for freelancer work, but you can contact me
+                        anyway and say hello.
                     </p>
                 </div>
             </div>
--- src/sections/Work.tsx
+++ src/sections/Work.tsx
@@ -2,16 +2,38 @@ import WorkShowcase from "../components/WorkShowcase";
 
 const Work = () => {
     return (
-        <div className="work-section">
+        <div className="section work-section">
             <h2 className="text-section">Work</h2>
             <h3 className="inner-header">A Selection Of Ideas that I Built</h3>
+            <p>
+                Some of my favorite ideas and projects that I have been a part
+                of
+            </p>
             <div className="work-showcase">
-                <WorkShowcase title="Pay Fast" description="Pay Fast is a web application that serves as an exemplification model of a commercial payment gateway, making use of design patterns such as 'Circuit Breaker' on React, Express, Prisma and MySQL as a final project of the subject Software Design II." orientation="left" image="" link="https://github.com/AlejandroBlanco2001/PayFast"/>
-                <WorkShowcase title="Attendance System" description="Attendance System is a web application built on Express, React and MariaDB as a final project of the subject 'Database' for taking attendance (on-site, remote and hybrid) in university classrooms by scanning QR codes to reduce paper use according to Zero Paper Policy. " orientation="right" image="" link="https://github.com/AlejandroBlanco2001/AttendanceSystem" />
-                <WorkShowcase title="Langton's Ant" description="Langton's ant is a web application that simulates the universal Turing machine of Langton's ant proposed by Chris Langton in 1986, developed on Typescript, HTML and CSS." orientation="left" image="" link="https://github.com/AlejandroBlanco2001/Langton-s-ant" />
+                <WorkShowcase
+                    title="Pay Fast"
+                    description="Pay Fast is a web application that serves as an exemplification model of a commercial payment gateway, making use of design patterns such as 'Circuit Breaker' on React, Express, Prisma and MySQL as a final project of the subject Software Design II."
+                    orientation="left"
+                    image=""
+                    link="https://github.com/AlejandroBlanco2001/PayFast"
+                />
+                <WorkShowcase
+                    title="Attendance System"
+                    description="Attendance System is a web application built on Express, React and MariaDB as a final project of the subject 'Database' for taking attendance (on-site, remote and hybrid) in university classrooms by scanning QR codes to reduce paper use according to Zero Paper Policy. "
+                    orientation="right"
+                    image=""
+                    link="https://github.com/AlejandroBlanco2001/AttendanceSystem"
+                />
+                <WorkShowcase
+                    title="Langton's Ant"
+                    description="Langton's ant is a web application that simulates the universal Turing machine of Langton's ant proposed by Chris Langton in 1986, developed on Typescript, HTML and CSS."
+                    orientation="left"
+                    image=""
+                    link="https://github.com/AlejandroBlanco2001/Langton-s-ant"
+                />
             </div>
         </div>
-    )
+    );
 };
 
 export default Work;
